9aa061c2155921da6aa9bd282598be1d2ceb1549
Maria Eduarda <eduardabritto2014@gmail.com>
2026-06-03 03:47:43 -0300

    Refactor loadCSV method to load from startup_funding.csv

--- Financiamento/src/main/java/com/checkr/interviews/CsvLoader.java
+++ Financiamento/src/main/java/com/checkr/interviews/CsvLoader.java
@@ -8,15 +8,16 @@ import java.util.List;
 import com.opencsv.CSVReader;
 
 public class CsvLoader {
-    public static List<String[]> loadCSV(String filename) throws IOException {
-        List<String[]> csvData = new ArrayList<>();
-        CSVReader reader = new CSVReader(new FileReader(filename));
+
+    public static List<String[]> load() throws IOException {
+        List<String[]> csvData = new ArrayList<String[]>();
+        CSVReader reader = new CSVReader(new FileReader("startup_funding.csv"));
         String[] row = null;
 
         while((row = reader.readNext()) != null) {
             csvData.add(row);
         }
-
+        
         reader.close();
         csvData.remove(0);
         
